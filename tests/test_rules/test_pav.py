43f754b9a79b95d6c0c0270f8ef445b15286e131
Simon-Rey <simon.j.rey@proton.me>
2025-10-02 12:27:24 +0200

    Flexibility in the PAV ILP construction

--- tests/test_rules/test_pav.py
+++ tests/test_rules/test_pav.py
@@ -6,7 +6,7 @@ from tests.random_instances import get_random_profile
 from trivoting.election.alternative import Alternative
 from trivoting.election.trichotomous_ballot import TrichotomousBallot
 from trivoting.election.trichotomous_profile import TrichotomousProfile
-from trivoting.rules.pav import proportional_approval_voting
+from trivoting.rules.pav import proportional_approval_voting, PAVILPHervouin2025, PAVILPTalmonPage2021, PAVILPKraiczy2025
 from trivoting.election.selection import Selection
 
 
@@ -14,28 +14,30 @@ class TestPAV(TestCase):
 
     def test_pav_on_random_instance(self):
         for _ in range(20):
-            profile = get_random_profile(50, 100)
-            print(f"Computing PAV on randomly generated instance: {profile}")
-            max_size = random.randint(1, len(profile.alternatives))
-            res = proportional_approval_voting(profile, max_size, resoluteness=True)
-            self.assertLessEqual(len(res), max_size)
+            for builder in [PAVILPKraiczy2025, PAVILPTalmonPage2021, PAVILPHervouin2025]:
+                profile = get_random_profile(50, 100)
+                print(f"Computing PAV with {builder.__name__} on randomly generated instance: {profile}")
+                max_size = random.randint(1, len(profile.alternatives))
+                res = proportional_approval_voting(profile, max_size, ilp_builder_class=builder, resoluteness=True)
+                self.assertLessEqual(len(res), max_size)
 
     def test_pav_on_trivial_instances(self):
-        # Empty profile
-        profile = TrichotomousProfile()
-        self.assertEqual(proportional_approval_voting(profile, 0), Selection())
+        for builder in [PAVILPKraiczy2025, PAVILPTalmonPage2021, PAVILPHervouin2025]:
+            # Empty profile
+            profile = TrichotomousProfile()
+            self.assertEqual(proportional_approval_voting(profile, 0, ilp_builder_class=builder), Selection())
 
-        # Only disapproved
-        alternatives = [Alternative(i) for i in range(10)]
-        negative_ballots = [TrichotomousBallot(disapproved=alternatives[:6]) for _ in range(10)]
-        profile = TrichotomousProfile(negative_ballots, alternatives=alternatives)
-        res = proportional_approval_voting(profile, len(alternatives))
-        self.assertEqual(len(res), 0)
+            # Only disapproved
+            alternatives = [Alternative(i) for i in range(10)]
+            negative_ballots = [TrichotomousBallot(disapproved=alternatives[:6]) for _ in range(10)]
+            profile = TrichotomousProfile(negative_ballots, alternatives=alternatives)
+            res = proportional_approval_voting(profile, len(alternatives), ilp_builder_class=builder)
+            self.assertEqual(len(res), 0)
 
-        # Separated ballots
-        positive_ballots = [TrichotomousBallot(approved=alternatives[6:]) for _ in range(100)]
-        profile.extend(positive_ballots)
-        res = proportional_approval_voting(profile, len(alternatives))
-        self.assertEqual(len(res), 4)
-        for alt in alternatives[6:]:
-            self.assertIn(alt, res)
+            # Separated ballots
+            positive_ballots = [TrichotomousBallot(approved=alternatives[6:]) for _ in range(100)]
+            profile.extend(positive_ballots)
+            res = proportional_approval_voting(profile, len(alternatives), ilp_builder_class=builder)
+            self.assertEqual(len(res), 4)
+            for alt in alternatives[6:]:
+                self.assertIn(alt, res)
--- trivoting/rules/pav.py
+++ trivoting/rules/pav.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+import abc
+from collections.abc import Iterable, Callable
+
 from trivoting.election.trichotomous_profile import AbstractTrichotomousProfile
 
-from pulp import LpProblem, LpMaximize, LpBinary, LpVariable, lpSum, LpStatusOptimal, value, PULP_CBC_CMD
+from pulp import LpProblem, LpMaximize, LpBinary, LpVariable, lpSum, LpStatusOptimal, value, PULP_CBC_CMD, \
+    LpAffineExpression
 
 from trivoting.election.selection import Selection
 
@@ -27,19 +31,126 @@ class PAVMipVoter:
         The ballot of the voter.
     multiplicity : int
         The number of identical ballots represented by this voter.
-    x_vars : dict[int, LpVariable]
-        Decision variables representing the voter's contribution to the PAV score
-        for each possible approval count.
+    app_sat_vars : dict[int, LpVariable]
+        Decision variables counting the number of approved alternative that have been selected. The higher, the better.
+    disapp_sat_vars : dict[int, LpVariable]
+        Decision variables counting the number of disapproved alternative that have not been selected. The higher, the better.
+    app_dissat_vars : dict[int, LpVariable]
+        Decision variables counting the number of approved alternative that have not been selected. The higher, the worst.
+    disapp_dissat_vars : dict[int, LpVariable]
+        Decision variables counting the number of disapproved alternative that have been selected. The higher, the worst.
     """
     def __init__(self, ballot, multiplicity):
         self.ballot = ballot
         self.multiplicity = multiplicity
-        self.x_vars = dict()
+        self.app_sat_vars = None
+        self.disapp_sat_vars = None
+        self.app_dissat_vars = None
+        self.disapp_dissat_vars = None
+        self.sat_vars = None
+        self.dissat_vars = None
 
+class PAVILPBuilder(abc.ABC):
+    def __init__(self, profile: AbstractTrichotomousProfile, max_size_selection: int):
+        self.profile = profile
+        self.max_size_selection = max_size_selection
+        self.model = LpProblem("pav", LpMaximize)
+        self.voters = []
+        self.selection_vars = {alt: LpVariable(f"y_{alt.name}", cat=LpBinary) for alt in profile.alternatives}
+
+    @abc.abstractmethod
+    def init_voters_vars(self) -> None:
+        """"""
+
+    @abc.abstractmethod
+    def objective(self) -> LpAffineExpression:
+        """"""
+
+class PAVILPKraiczy2025(PAVILPBuilder):
+    """
+    Defines the ILP objective for the PAV ILP solver as defined in Section 3.3 of
+    ``Proportionality in Thumbs Up and Down Voting`` (Kraiczy, Papasotiropoulos, Pierczyński and Skowron, 2025).
+    The objective is to maximise the PAV score where both approved and selected, and disapproved and not selected
+    alternatives contribute positively.
+    """
+    def init_voters_vars(self) -> None:
+        # Init the variables
+        for i, ballot in enumerate(self.profile):
+            pav_voter = PAVMipVoter(ballot, multiplicity=self.profile.multiplicity(ballot))
+            pav_voter.sat_vars = dict()
+            for k in range(1, len(self.profile.alternatives) + 1):
+                pav_voter.sat_vars[k] = LpVariable(f"s_{i}_{k}", cat=LpBinary)
+            self.voters.append(pav_voter)
+
+        # Constraint them to ensure proper counting
+        for voter in self.voters:
+            self.model += lpSum(voter.sat_vars.values()) == lpSum(
+                self.selection_vars[alt] for alt in voter.ballot.approved) + lpSum(
+                1 - self.selection_vars[alt] for alt in voter.ballot.disapproved)
+
+    def objective(self) -> LpAffineExpression:
+        return lpSum(lpSum(v / i for i, v in voter.sat_vars.items()) for voter in self.voters)
+
+
+class PAVILPTalmonPage2021(PAVILPBuilder):
+    """
+    Defines the ILP objective for the PAV ILP solver as defined in Section 3.3 of
+    ``Proportionality in Committee Selection with Negative Feelings`` (Talmon and Page, 2021).
+    The objective is to maximise the difference between (1) the PAV score in which approved and selected alternatives
+    are taken into account, and (2) the PAV score in which disapproved but selected alternatives are taken into account.
+    """
+
+    def init_voters_vars(self) -> None:
+        # Init the variables
+        for i, ballot in enumerate(self.profile):
+            pav_voter = PAVMipVoter(ballot, multiplicity=self.profile.multiplicity(ballot))
+            pav_voter.app_sat_vars = dict()
+            pav_voter.disapp_dissat_vars = dict()
+            for k in range(1, len(self.profile.alternatives) + 1):
+                pav_voter.app_sat_vars[k] = LpVariable(f"as_{i}_{k}", cat=LpBinary)
+                pav_voter.disapp_dissat_vars[k] = LpVariable(f"dd_{i}_{k}", cat=LpBinary)
+            self.voters.append(pav_voter)
+
+        # Constraint them to ensure proper counting
+        for voter in self.voters:
+            self.model += lpSum(voter.app_sat_vars.values()) == lpSum(self.selection_vars[alt] for alt in voter.ballot.approved)
+            self.model += lpSum(voter.disapp_dissat_vars.values()) == lpSum(self.selection_vars[alt] for alt in voter.ballot.disapproved)
+
+    def objective(self) -> LpAffineExpression:
+        return lpSum(lpSum(v / i for i, v in voter.app_sat_vars.items()) for voter in self.voters) - lpSum(lpSum(v / i for i, v in voter.disapp_dissat_vars.items()) for voter in self.voters)
+
+
+class PAVILPHervouin2025(PAVILPBuilder):
+    """
+    Defines the ILP objective for the PAV ILP solver as defined by Matthieu Hervouin.
+    The objective is to maximise the sum of (1) the PAV score in which approved and selected alternatives
+    are taken into account, and (2) the PAV score over the maximum size of the selection minus the number of
+    disapproved but selected alternatives.
+    """
+
+    def init_voters_vars(self) -> None:
+        # Init the variables
+        for i, ballot in enumerate(self.profile):
+            pav_voter = PAVMipVoter(ballot, multiplicity=self.profile.multiplicity(ballot))
+            pav_voter.app_sat_vars = dict()
+            pav_voter.disapp_dissat_vars = dict()
+            for k in range(1, len(self.profile.alternatives) + 1):
+                pav_voter.app_sat_vars[k] = LpVariable(f"as_{i}_{k}", cat=LpBinary)
+                pav_voter.disapp_dissat_vars[k] = LpVariable(f"dd_{i}_{k}", cat=LpBinary)
+            self.voters.append(pav_voter)
+
+        # Constraint them to ensure proper counting
+        for voter in self.voters:
+            self.model += lpSum(voter.app_sat_vars.values()) == lpSum(self.selection_vars[alt] for alt in voter.ballot.approved)
+            self.model += lpSum(voter.disapp_dissat_vars.values()) == self.max_size_selection - lpSum(self.selection_vars[alt] for alt in voter.ballot.disapproved)
+
+    def objective(self) -> LpAffineExpression:
+        return lpSum(lpSum(v / i for i, v in voter.app_sat_vars.items()) for voter in self.voters) + lpSum(lpSum(v / i for i, v in voter.disapp_dissat_vars.items()) for voter in self.voters)
 
 def proportional_approval_voting(
     profile: AbstractTrichotomousProfile,
     max_size_selection: int,
+    ilp_builder_class: type[PAVILPBuilder] = None,
     initial_selection: Selection | None = None,
     resoluteness: bool = True,
     verbose: bool = False,
@@ -49,8 +160,7 @@ def proportional_approval_voting(
     Compute the selections of the Proportional Approval Voting (PAV) rule via Integer Linear Programming (ILP).
     The ILP is solved with the `pulp` package.
 
-    The definition of the PAV rule for the trichotomous context is taken from Section 3.3 of
-    ``Proportionality in Thumbs Up and Down Voting`` (Kraiczy, Papasotiropoulos, Pierczyński and Skowron, 2025).
+    Different objective functions can be used.
 
     Parameters
     ----------
@@ -58,6 +168,8 @@ def proportional_approval_voting(
         The trichotomous profile.
     max_size_selection : int
         Maximum number of alternatives to select.
+    ilp_builder_class : type[PAVILPBuilder], optional
+        Builder class for the ILP. Defautls to :py:class:`PAVILPKraiczy2025`.
     initial_selection : Selection, optional
         An initial partial selection fixing some alternatives as selected or rejected.
         If `implicit_reject` is True in the initial selection, no alternatives are fixed to be rejected.
@@ -80,72 +192,67 @@ def proportional_approval_voting(
         if irresolute (:code:`resoluteness == False`).
     """
 
-    mip_model = LpProblem("pav", LpMaximize)
+    if ilp_builder_class is None:
+        ilp_builder_class = PAVILPKraiczy2025
 
-    pav_voters = []
-    for i, ballot in enumerate(profile):
-        pav_voter = PAVMipVoter(ballot, multiplicity=profile.multiplicity(ballot))
-        for k in range(1, len(profile.alternatives) + 1):
-            pav_voter.x_vars[k] = LpVariable(f"x_{i}_{k}", cat=LpBinary)
-        pav_voters.append(pav_voter)
+    ilp_builder = ilp_builder_class(profile, max_size_selection)
 
-    y_vars = {alt: LpVariable(f"y_{alt.name}", cat=LpBinary) for alt in profile.alternatives}
+    model = ilp_builder.model
+    ilp_builder.init_voters_vars()
+    selection_vars = ilp_builder.selection_vars
 
     # Select no more than allowed
-    mip_model += lpSum(y_vars.values()) <= max_size_selection
+    model += lpSum(selection_vars.values()) <= max_size_selection
 
-    # Counts for the voters correspond to approved and selected + disapproved and not selected
-    for voter in pav_voters:
-        mip_model += lpSum(voter.x_vars.values()) == lpSum(y_vars[alt] for alt in voter.ballot.approved) + lpSum(1 - y_vars[alt] for alt in voter.ballot.disapproved)
-
+    # Handle initial selection
     if initial_selection is not None:
         for alt in initial_selection.selected:
-            mip_model += y_vars[alt] == 1
+            model += selection_vars[alt] == 1
         if not initial_selection.implicit_reject:
             for alt in initial_selection.rejected:
-                mip_model += y_vars[alt] == 0
+                model += selection_vars[alt] == 0
 
     # Objective: max PAV score
-    mip_model += lpSum(lpSum(v / i for i, v in voter.x_vars.items()) for voter in pav_voters)
+    model += ilp_builder.objective()
 
-    status = mip_model.solve(PULP_CBC_CMD(msg=verbose, timeLimit=max_seconds))
+    status = model.solve(PULP_CBC_CMD(msg=verbose, timeLimit=max_seconds))
 
     all_selections = []
 
     if status == LpStatusOptimal:
         selection = Selection(implicit_reject=True)
-        for alt, v in y_vars.items():
+        for alt, v in selection_vars.items():
             if value(v) >= 0.9:
                 selection.add_selected(alt)
         all_selections.append(selection)
     else:
-        raise ValueError("Solver did not find an optimal solution.")
+        raise ValueError(f"Solver did not find an optimal solution, status is {status}.")
 
     if resoluteness:
         return all_selections[0]
 
     # If irresolute, we solve again, banning the previous selections
-    mip_model += lpSum(lpSum(v / i for i, v in voter.x_vars.items()) for voter in pav_voters) == value(mip_model.objective)
+    model += ilp_builder.objective() == value(model.objective)
 
     previous_selection = selection
     while True:
         # See http://yetanothermathprogrammingconsultant.blogspot.com/2011/10/integer-cuts.html
-        mip_model += (
-                             lpSum((1 - y_vars[a]) for a in previous_selection.selected) +
-                             lpSum(y_vars[a] for a in y_vars if a not in previous_selection)
+        model += (
+                             lpSum((1 - selection_vars[a]) for a in previous_selection.selected) +
+                             lpSum(selection_vars[a] for a in selection_vars if a not in previous_selection)
                      ) >= 1
 
-        mip_model += (
-                             lpSum(y_vars[a] for a in previous_selection.selected) -
-                             lpSum(y_vars[a] for a in y_vars if a not in previous_selection)
+        model += (
+                             lpSum(selection_vars[a] for a in previous_selection.selected) -
+                             lpSum(selection_vars[a] for a in selection_vars if a not in previous_selection)
                      ) <= len(previous_selection) - 1
 
-        status = mip_model.solve(PULP_CBC_CMD(msg=verbose, timeLimit=max_seconds))
+        status = model.solve(PULP_CBC_CMD(msg=verbose, timeLimit=max_seconds))
 
         if status != LpStatusOptimal:
             break
 
-        previous_selection = Selection([a for a in y_vars if value(y_vars[a]) is not None and value(y_vars[a]) >= 0.9], implicit_reject=True)
+        previous_selection = Selection([a for a in selection_vars if value(selection_vars[a]) is not None and value(selection_vars[a]) >= 0.9], implicit_reject=True)
         if previous_selection not in all_selections:
             all_selections.append(previous_selection)
 
