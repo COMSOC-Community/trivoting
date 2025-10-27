02c989df8a2704d897de05187361f53f73f39c38
Simon-Rey <simon.j.rey@proton.me>
2025-10-27 15:22:12 +0100

    Tests for CC

---
+++ tests/test_rules/test_chamberlin_courant.py
@@ -0,0 +1,51 @@
+import random
+from unittest import TestCase
+
+from tests.random_instances import get_random_profile
+from trivoting.election import TrichotomousProfile, Selection, Alternative, TrichotomousBallot
+from trivoting.rules.chamberlin_courant import chamberlin_courant
+
+
+class TestChamberlinCourant(TestCase):
+
+    def test_cc_on_random_instance(self):
+        for _ in range(50):
+            profile = get_random_profile(50, 100)
+            print(f"Computing CC on randomly generated instance: {profile}")
+            max_size = random.randint(1, len(profile.alternatives))
+            res = chamberlin_courant(profile, max_size, resoluteness=True)
+            self.assertLessEqual(len(res), max_size)
+
+    def test_cc_on_trivial_instances(self):
+        # Empty profile
+        profile = TrichotomousProfile()
+        self.assertEqual(chamberlin_courant(profile, 0), Selection())
+
+        # Only disapproved
+        alternatives = [Alternative(i) for i in range(10)]
+        negative_ballots = [
+            TrichotomousBallot(disapproved=alternatives[:6]) for _ in range(10)
+        ]
+        profile = TrichotomousProfile(negative_ballots, alternatives=alternatives)
+        res = chamberlin_courant(profile, len(alternatives))
+        self.assertEqual(len(res), 0)
+
+    def test_cc_on_specific_instances(self):
+        a = Alternative("a")
+        b = Alternative("b")
+        c = Alternative("c")
+        d = Alternative("d")
+        e = Alternative("e")
+        alternatives = [a, b, c, d, e]
+        ballots = [
+            TrichotomousBallot(approved=[a, b], disapproved=[c]),
+            TrichotomousBallot(approved=[c]),
+        ]
+        profile = TrichotomousProfile(ballots, alternatives=alternatives)
+        res = chamberlin_courant(profile, 3)
+        self.assertEqual(len(res), 3)
+
+        profile.add_ballot(TrichotomousBallot(approved=[a], disapproved=[c]))
+        res = chamberlin_courant(profile, 2)
+        self.assertIn(a, res)
+        self.assertNotIn(c, res)
